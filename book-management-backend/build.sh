770e179a251b3d5145f6d177c1f627b0006d90d2
yuxin.wang <1090071876@qq.com>
2024-04-24 18:01:54 +0800

    first commit

---
+++ book-management-backend/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p dist
+
+# 编译 Java 代码
+javac -d dist src/**/*.java
---
+++ book-management-backend/src/main/java/com/example/Book.java
@@ -0,0 +1,56 @@
+package com.example;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private Integer publicationYear;
+    private String isbn;
+
+    // Getters and setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getPublicationYear() {
+        return this.publicationYear;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+}
